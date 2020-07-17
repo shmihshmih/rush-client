edb250a9193616dd7aa7ed4443eb9b6ca261cb55
shmihshmih <shmihshmih@gmail.com>
2020-07-17 17:22:00 +0300

    start input

--- src/parts/AuthPart/Auth.js
+++ src/parts/AuthPart/Auth.js
@@ -5,10 +5,64 @@ import {hideAuth, showAuth} from '../../station/action'
 import ReactDOM from 'react-dom'
 
 function Auth(props) {
+  //TODO harcoded change to bd
+  const users = [
+    {
+      name: 'Dodik',
+      email: "dodik@protonmail.com",
+      avatar: "https://picsum.photos/200/200?random=3",
+      isDriver: false,
+      isPedestrian: true,
+      about: "Ya takoy klassniy, horosho razgadivayui zagadki da i viibshe umnik hot, kuda, chmafff'",
+      birth: "16-05-1992",
+      rate: "25",
+      car: "Chevrolet Cruise",
+      isCarVisible: true,
+      carNumber: "c250jv",
+      isCarNumberVisible: true
+    },
+    {
+      name: 'bobolik',
+      email: "bobolik@protonmail.com",
+      avatar: "https://picsum.photos/200/200?random=3",
+      isDriver: false,
+      isPedestrian: true,
+      about: "Ya takoy klassniy, horosho razgadivayui zagadki da i viibshe umnik hot, kuda, chmafff'",
+      birth: "16-05-1992",
+      rate: "25",
+      car: "Chevrolet Cruise",
+      isCarVisible: true,
+      carNumber: "c250jv",
+      isCarNumberVisible: true
+    },
+    {
+      name: 'sokolik',
+      email: "sokolik@protonmail.com",
+      avatar: "https://picsum.photos/200/200?random=3",
+      isDriver: false,
+      isPedestrian: true,
+      about: "Ya takoy klassniy, horosho razgadivayui zagadki da i viibshe umnik hot, kuda, chmafff'",
+      birth: "16-05-1992",
+      rate: "25",
+      car: "Chevrolet Cruise",
+      isCarVisible: true,
+      carNumber: "c250jv",
+      isCarNumberVisible: true
+    }
+  ]
+
   const [authStatus, setAuthStatus] = useState(props.isAuthOpenReducer);
 
-  function modalHandler() {
-    authStatus.isAuthOpen ? props.hideAuth() : props.showAuth()
+  function modalHandler(e) {
+    let fod = users.find( user => user.email == "sokolik@protonmail.com")
+    fod ? console.log('est') : console.log('nonono');
+    setInterval( () => {
+      authStatus.isAuthOpen ? props.hideAuth() : props.showAuth()
+    }, 2000)
+  }
+
+  function emailInputHandler(e) {
+    console.log(e.target.value)
   }
 
   useEffect(() => {
@@ -24,7 +78,7 @@ function Auth(props) {
             <form className="col s12">
               <div className="row">
                 <div className="input-field col s12">
-                  <input id="email" type="email" className="validate"/>
+                  <input id="email" type="email" className="validate" onChange={emailInputHandler}/>
                   <label htmlFor="email">Email</label>
                   <span className="helper-text" data-error="wrong" data-success="right">
                       Введите свою электронную почту
